754bdfbc07970c37dffaad47f91a3b9e3c645055
Oscar Silva <oscarsilva2356@gmail.com>
2022-06-23 09:16:55 -0700

    Made Getter and Setters

--- src/main/java/com/example/MadLib.java
+++ src/main/java/com/example/MadLib.java
@@ -1,10 +1,112 @@
 package com.example;
 
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class MadLib {
     Scanner scan = new Scanner(System.in);
+    String story;
+    String name;
+    String adjective1;
+    String adjective2;
+    String adjective3;
+    String noun1;
+    String noun2;
+    String noun3;
+    String adverb;
+    String randomNums;
+    Random rand = new Random();
+
+    // Getters!
+    public Scanner getScan() {
+        return scan;
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAdjective1() {
+        return adjective1;
+    }
+
+    public String getAdjective2() {
+        return adjective2;
+    }
+
+    public String getAdjective3() {
+        return adjective3;
+    }
+
+    public String getNoun1() {
+        return noun1;
+    }
+
+    public String getNoun2() {
+        return noun2;
+    }
+
+    public String getNoun3() {
+        return noun3;
+    }
+
+    public String getAdverb() {
+        return adverb;
+    }
+
+    public String getRandomNums() {
+        return randomNums;
+    }
+
+    //Setter!
+    public void setScan(Scanner scan) {
+        this.scan = scan;
+    }
+
+    public void setStory(String story) {
+        this.story = story;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAdjective1(String adjective1) {
+        this.adjective1 = adjective1;
+    }
+
+    public void setAdjective2(String adjective2) {
+        this.adjective2 = adjective2;
+    }
+
+    public void setAdjective3(String adjective3) {
+        this.adjective3 = adjective3;
+    }
+
+    public void setNoun1(String noun1) {
+        this.noun1 = noun1;
+    }
+
+    public void setNoun2(String noun2) {
+        this.noun2 = noun2;
+    }
+
+    public void setNoun3(String noun3) {
+        this.noun3 = noun3;
+    }
+
+    public void setAdverb(String adverb) {
+        this.adverb = adverb;
+    }
+
+    public void setRandomNums(String randomNums) {
+        this.randomNums = randomNums;
+    }
 
     public static void main(String[] args)  {
 
